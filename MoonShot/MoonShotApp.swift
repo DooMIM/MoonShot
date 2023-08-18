7089389b9cf385713890058a84fbcc02c7dee026
Doha <doha.moustafa@hotmail.com>
2023-08-18 13:26:19 +0200

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by doha moustafa on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
